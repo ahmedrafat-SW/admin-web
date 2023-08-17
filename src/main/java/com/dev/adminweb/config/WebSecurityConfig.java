5389ac0a2859adc67aec28c847cf6d7810f819bf
Ahmed Rafat <ahmed.rafat.abdelbar@gmail.com>
2023-08-17 20:27:54 +0300

    update userDetails role and password

--- src/main/java/com/dev/adminweb/config/WebSecurityConfig.java
+++ src/main/java/com/dev/adminweb/config/WebSecurityConfig.java
@@ -2,19 +2,19 @@ package com.dev.adminweb.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-
-import javax.sql.DataSource;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -22,19 +22,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/", "/home").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .authenticationManager(authenticationManager())
                 .httpBasic();
     }
 
-    @Bean
-    public UserDetailsService users(DataSource dataSource ){
-        return new JdbcUserDetailsManager(dataSource);
-    }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("user")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
     }
-
-
 }
