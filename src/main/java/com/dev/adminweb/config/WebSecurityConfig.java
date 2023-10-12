b4f50ac10a2ea4a32aae924254d6c2a2c3a8ba38
Ahmed Rafat <ahmed.rafat.abdelbar@gmail.com>
2023-10-12 15:39:44 +0300

    update security configurations

--- src/main/java/com/dev/adminweb/config/WebSecurityConfig.java
+++ src/main/java/com/dev/adminweb/config/WebSecurityConfig.java
@@ -2,8 +2,6 @@ package com.dev.adminweb.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -26,7 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .httpBasic();
     }
-
+// do some commits
 
     @Bean
     public UserDetailsService userDetailsService() {
